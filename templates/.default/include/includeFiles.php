9bb357c021dcae85525f4002329d4f95256755c9
fatal1ty161 <88760129+fatal1ty161@users.noreply.github.com>
2023-02-15 12:12:45 +0300

    Delete includeFiles.php

--- templates/.default/include/includeFiles.php
+++
@@ -1,9 +0,0 @@
-<?
-
-global $USER;
-$this->setFrameMode(true);
-$this->addExternalCss($templateFolder . '/style.css');
-$this->addExternalJs($templateFolder . '/scripts.js');
-$request = \Bitrix\Main\Context::getCurrent()->getRequest();
-
-$templatePath = '/local/components/UW/calc/templates/.default/images/';
